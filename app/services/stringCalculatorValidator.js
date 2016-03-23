bce6c36c3a35cedcfec4ea0bd1187868ff0b6218
Mario Vargas <mvargas@cardinalsolutions.com>
2016-03-23 11:11:06 -0400

    Moved the ensureNoNegatives() helper function outside of the StringCalculatorValidator.validateWithError() function because it can exist in the same scope as the StringCalculatorValidator class.

--- app/services/stringCalculatorValidator.js
+++ app/services/stringCalculatorValidator.js
@@ -8,15 +8,15 @@
 
     StringCalculatorValidator.prototype.validateWithError = function (numbersToValidate) {
         ensureNoNegatives(numbersToValidate);
-
-        function ensureNoNegatives(numberList) {
-            var negativeNumbers = numberList.filter(function (n) {
-                return n < 0;
-            });
-
-            if (negativeNumbers.length) {
-                throw new Error("Negatives not allowed: " + negativeNumbers.join(", "));
-            }
-        }
     };
+
+    function ensureNoNegatives(numberList) {
+        var negativeNumbers = numberList.filter(function (n) {
+            return n < 0;
+        });
+
+        if (negativeNumbers.length) {
+            throw new Error("Negatives not allowed: " + negativeNumbers.join(", "));
+        }
+    }
 }());
